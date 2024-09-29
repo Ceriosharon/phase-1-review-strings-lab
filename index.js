2e8f5a7f4e3bad844e4e809f028842e52dc1a051
ceriosharon <ccerio73@gmail.com>
2024-09-29 17:25:26 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code in this file!
+let currentUser = 'Grace Hopper';
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+let shortGreeting = `Welcome, ${currentUser[0]}!`;
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+
+
+
+
+
+
